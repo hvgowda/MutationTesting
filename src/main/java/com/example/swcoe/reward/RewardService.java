58f53bffc4070ac10c7521390ef74d98ad63fe88
Harsha <harshavardhana.gowda@philips.com>
2019-09-12 10:33:48 +0530

    Demo

---
+++ src/main/java/com/example/swcoe/reward/RewardService.java
@@ -0,0 +1,30 @@
+package com.example.swcoe.reward;
+
+import com.example.swcoe.product.Product;
+
+import java.util.List;
+
+public abstract class RewardService {
+    protected long neededPoints;
+
+    public abstract RewardInformation applyReward(
+            List<Product> order, long customerPoints);
+
+    protected double calculateTotal(List<Product> order) {
+        double sum = 0;
+
+        if(order != null) {
+            sum = order.stream().mapToDouble(Product::getPrice).sum();
+        }
+
+        return sum;
+    }
+
+    public long getNeededPoints() {
+        return neededPoints;
+    }
+
+    public void setNeededPoints(long neededPoints) {
+        this.neededPoints = neededPoints;
+    }
+}
